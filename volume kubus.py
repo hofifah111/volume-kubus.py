a9b286e5b19022876436e644219ec91cfd3d0844
hofifah111 <113626444+hofifah111@users.noreply.github.com>
2022-09-15 23:48:22 +0700

    Add files via upload

---
+++ volume kubus.py
@@ -0,0 +1,34 @@
+#PROGRAM MENGHITUNG VOLUME KUBUS DINAMIS
+
+
+print("PROGRAM PHYTON VOLUME KUBUS")
+s = float(input("nilai sisi"))
+v = s*s*s*s*s
+print ("volume kubus",v) 
+
+
+
+#PROGRAM MENGHITUNG VOLUME KUBUS STATIS
+s = 10
+v = s*s*s*s*s
+print ("volume kubus",v)
+
+
+
+#PROGRAM MENGHITUNG VOLUME BALOK DINAMIS
+
+print("PROGRAM PHYTON MENGHITUNG VOLUME")
+P = float(input("Masukan Nilai Panjang = "))
+l = float(input("Masukan Nilai lebar = "))
+t = float(input("Masukan Nilai tinggi = "))
+v = P*l*t
+print("Volume Balok =",v)
+
+
+
+#PROGRAM MENGHITUNG VOLUME BALOK STATIS
+P = 15
+l = 5
+t = 20
+v = P*l*t
+print ("Volume Balok =",v)
